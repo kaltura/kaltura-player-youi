abe0da9e7a632827c9f02391c1b6ffa9c0bc59d3
Nilit Danan <nilit.danan@kaltura.com>
2020-10-23 18:02:46 +0300

    Updated updatePlaykitSdkBridge script.

--- scripts/updatePlaykitSdkBridge.pl
+++ scripts/updatePlaykitSdkBridge.pl
@@ -22,19 +22,22 @@ print ("---------------------\n\n");
 print ("FullPathToTvFolder => $fullPathToTvFolder\n");
 
 chdir "$playkitBridgeScripts/../";
+my $currentDir =  `pwd`;
+print ("CurrentDir => $currentDir\n");
 
 youiTvRun();
 
-chdir "$playkitBridgeScripts/../youi/";
+chdir "$playkitBridgeScripts/youi/";
 my $currentDir =  `pwd`;
-print "CopyFromDir => $currentDir\n";
+print ("CopyFromDir => $currentDir\n");
 
 copyCmake();
 copyHFiles();
 copyCPPFiles();
 copyJavaFiles();
-copyIOSModules();
 copyMMFiles();
+copyIOSModules();
+copyTvOSModules();
 updatePlaykitVersion();
 
 print ("\nUpdate DONE!\n");
@@ -42,23 +45,25 @@ print "------------\n\n";
 
 sub copyCmake {
     print "\nCOPY CMAKE\n";
-    print "-------------\n";
+    print "----------\n";
     ## will be used in iOS integration
 
     print ("mkdir -p $fullPathToTvFolder/youi/cmake/Platform/\n");
     system ("mkdir -p $fullPathToTvFolder/youi/cmake/Platform/");
 
-    print ("cp ./cmake/Platform/YiIos.cmake  $fullPathToTvFolder/youi/cmake/Platform/\n");
-    system ("cp ./cmake/Platform/YiIos.cmake  $fullPathToTvFolder/youi/cmake/Platform/");
+    print ("cp -f ./cmake/Platform/YiIos.cmake  $fullPathToTvFolder/youi/cmake/Platform/\n");
+    system ("cp -f ./cmake/Platform/YiIos.cmake  $fullPathToTvFolder/youi/cmake/Platform/");
 
-
+    print ("cp -f ./cmake/Platform/YiTvos.cmake  $fullPathToTvFolder/youi/cmake/Platform/\n");
+    system ("cp -f ./cmake/Platform/YiTvos.cmake  $fullPathToTvFolder/youi/cmake/Platform/");
+    
     ##print ("cp ./SourceList.cmake  $fullPathToTvFolder/youi/\n");
     ##system ("cp ./SourceList.cmake  $fullPathToTvFolder/youi/");
 }
 
 sub youiTvRun {
-    print "\nYOUI TV RUN\n";
-    print "-------------\n";
+    print "\nYOUI TV RUN Android\n";
+    print "-------------------\n";
     system ("youi-tv generate -p android");
     system ("youi-tv run -p android");
 }
@@ -68,8 +73,8 @@ sub copyHFiles {
     print "------------\n";
     print ("mkdir -p $fullPathToTvFolder/youi/src/pk\n");
     system ("mkdir -p $fullPathToTvFolder/youi/src/pk");
-    print ("cp ./src/pk/*.h  $fullPathToTvFolder/youi/src/pk/\n");
-    system ("cp ./src/pk/*.h $fullPathToTvFolder/youi/src/pk/");
+    print ("cp -f ./src/pk/*.h  $fullPathToTvFolder/youi/src/pk/\n");
+    system ("cp -f ./src/pk/*.h $fullPathToTvFolder/youi/src/pk/");
 }
 
 sub copyCPPFiles {
@@ -77,8 +82,8 @@ sub copyCPPFiles {
     print "--------------\n";
     print ("mkdir -p $fullPathToTvFolder/youi/src/pk/android\n");
     system ("mkdir -p $fullPathToTvFolder/youi/src/pk/android");
-    print ("cp ./src/pk/android/*  $fullPathToTvFolder/youi/src/pk/android/\n");
-    system ("cp ./src/pk/android/*  $fullPathToTvFolder/youi/src/pk/android/");
+    print ("cp -f ./src/pk/android/*  $fullPathToTvFolder/youi/src/pk/android/\n");
+    system ("cp -f ./src/pk/android/*  $fullPathToTvFolder/youi/src/pk/android/");
 }
 
 sub copyJavaFiles {
@@ -88,8 +93,8 @@ sub copyJavaFiles {
     print "destJavaFolder = $destJavaFolder\n";
     print ("mkdir -p $destJavaFolder\n");
     system ("mkdir -p $destJavaFolder");
-    print ("cp -r ./android/modules/MyApp/src/main/java/tv/youi/myapp/*  $destJavaFolder/\n");
-    system("cp -r ./android/modules/MyApp/src/main/java/tv/youi/myapp/*  $destJavaFolder/");
+    print ("cp -rf ./android/modules/MyApp/src/main/java/tv/youi/myapp/*  $destJavaFolder/\n");
+    system("cp -rf ./android/modules/MyApp/src/main/java/tv/youi/myapp/*  $destJavaFolder/");
 }
 
 sub copyMMFiles {
@@ -99,21 +104,31 @@ sub copyMMFiles {
     print "destIOSFolder = $destIOSFolder\n";
     print ("mkdir -p $destIOSFolder\n");
     system ("mkdir -p $destIOSFolder");
-    print ("cp ./src/pk/ios/*  $destIOSFolder/\n");
-    system ("cp ./src/pk/ios/*  $destIOSFolder");
+    print ("cp -rf ./src/pk/ios/*  $destIOSFolder/\n");
+    system ("cp -rf ./src/pk/ios/*  $destIOSFolder");
 }
 
 sub copyIOSModules() {
     print "\nCOPY IOS Modules\n";
-    print "----------------\n\n";
+    print "----------------\n";
     print ("mkdir -p $fullPathToTvFolder/youi/ios-libs\n");
     system ("mkdir -p  $fullPathToTvFolder/youi/ios-libs");
-    system ("../scripts/ios-libs/download.sh && cp -r ./ios-libs/* $fullPathToTvFolder/youi/ios-libs/")
+    print ("cp -rf ./ios-libs/* $fullPathToTvFolder/youi/ios-libs/\n");
+    system ("cp -rf ./ios-libs/* $fullPathToTvFolder/youi/ios-libs/");
+}
+
+sub copyTvOSModules() {
+    print "\nCOPY TvOS Modules\n";
+    print "----------------\n";
+    print ("mkdir -p $fullPathToTvFolder/youi/tvos-libs\n");
+    system ("mkdir -p  $fullPathToTvFolder/youi/tvos-libs");
+    print ("cp -rf ./tvos-libs/* $fullPathToTvFolder/youi/tvos-libs/\n");
+    system ("cp -rf ./tvos-libs/* $fullPathToTvFolder/youi/tvos-libs/");
 }
 
 sub updatePlaykitVersion {
-    print "\nUPDATE PLAYKIT VER\n";
-    print "--------------------\n";
+    print "\nUPDATE PLAYKIT VER Android\n";
+    print "--------------------------\n";
 
    my $newVersion = `grep \"def kalturaPlaykitVersion\"  ./android/modules/MyApp/build.gradle.in`;
    chomp($newVersion);
