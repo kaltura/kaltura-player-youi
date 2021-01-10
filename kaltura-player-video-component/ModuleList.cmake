838be13a1e527182a301a5812f10d26e8f9f4b3e
Gilad Nadav <gilad.nadav@kaltura.com>
2021-01-10 16:04:40 +0200

    cont

--- kaltura-player-video-component/ModuleList.cmake
+++ kaltura-player-video-component/ModuleList.cmake
@@ -11,10 +11,15 @@ set(_MODULE_HEADERS ${_headers1} ${_headers2})
 file(GLOB _source1 "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
 file(GLOB _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
 set(_MODULE_SOURCE ${_source1} ${_source2})
+#set(KALTURA_PLAYER_ANDROID_PATH ${CMAKE_CURRENT_LIST_DIR})     
 
 # Make sure the specific platform include is processed
 if(IOS)
-    include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiIos.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiIos.cmake) 
+if(TVOS)
+    include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiTvos.cmake)
+if (ANDROID)
+    include(${CMAKE_CURRENT_LIST_DIR}/youi/android/modules/KalturaPlayer/src)
 endif()
 
 target_sources(${PROJECT_NAME} PRIVATE
