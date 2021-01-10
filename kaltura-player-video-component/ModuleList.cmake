689e167687e8eec4cc3c29c229545c50f654d258
Gilad Nadav <gilad.nadav@kaltura.com>
2021-01-10 19:57:32 +0200

    fix compilation part 1

--- kaltura-player-video-component/ModuleList.cmake
+++ kaltura-player-video-component/ModuleList.cmake
@@ -5,21 +5,27 @@ message(STATUS "Running: ${CMAKE_CURRENT_LIST_DIR}/ModuleList.cmake")
 set(_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/youi/src")
 
 file(GLOB _headers1 "${_MODULE_PATH}/nativemodule/*.h" "${_MODULE_PATH}/pk/*.h")
-file(GLOB _headers2 "${_MODULE_PATH}/player/ios/*.h")
+if(IOS OR TVOS) 
+	file(GLOB _headers2 "${_MODULE_PATH}/player/ios/*.h")
+endif()
 set(_MODULE_HEADERS ${_headers1} ${_headers2})
 
 file(GLOB _source1 "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
-file(GLOB _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
+if(IOS OR TVOS)
+	file(GLOB _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
+endif()
 set(_MODULE_SOURCE ${_source1} ${_source2})
 #set(KALTURA_PLAYER_ANDROID_PATH ${CMAKE_CURRENT_LIST_DIR})     
 
 # Make sure the specific platform include is processed
 if(IOS)
     include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiIos.cmake) 
+endif()
 if(TVOS)
     include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiTvos.cmake)
+endif()
 if (ANDROID)
-    include(${CMAKE_CURRENT_LIST_DIR}/youi/android/modules/KalturaPlayer/src)
+    include(${CMAKE_CURRENT_LIST_DIR}/android)
 endif()
 
 target_sources(${PROJECT_NAME} PRIVATE
