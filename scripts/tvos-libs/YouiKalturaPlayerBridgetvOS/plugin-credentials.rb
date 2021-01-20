d56357c33196e38769b30406457399b88052e81a
Nilit Danan <nilit.danan@kaltura.com>
2021-01-20 14:22:44 +0200

    FEC-10900 Updated the gitignore, which ignored the plugin-credentials files. Added the plugin-credentials files.

---
+++ scripts/tvos-libs/YouiKalturaPlayerBridgetvOS/plugin-credentials.rb
@@ -0,0 +1,10 @@
+#
+# Before 'pod install':
+#
+# Fill up login and password for broadpeak platform Git.
+#
+#
+# Do not commit 'plugin-credentials.rb' with credentials to Git repo!!!
+#
+@@broadpeak_platform_login = ''
+@@broadpeak_platform_password = ''
