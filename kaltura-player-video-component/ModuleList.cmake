82edd4b0d9a45af1c900aa041a3d01c88429175e
Gilad Nadav <gilad.nadav@kaltura.com>
2021-01-21 13:38:17 +0200

    add missing Download files in pk folder

--- kaltura-player-video-component/ModuleList.cmake
+++ kaltura-player-video-component/ModuleList.cmake
@@ -6,23 +6,30 @@ set(_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/youi/src")
 set(KALTURA_PLAYER_JAVA_SRC_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 file(GLOB_RECURSE _headers1 "${_MODULE_PATH}/nativemodule/*.h" "${_MODULE_PATH}/pk/*.h")
+
 if(IOS OR TVOS)
 	file(GLOB_RECURSE _headers2 "${_MODULE_PATH}/player/ios/*.h")
 	set(_MODULE_HEADERS ${_headers1} ${_headers2})
 endif()
+
 if(ANDROID)
 	set(_MODULE_HEADERS ${_headers1})
-endif()
-file(GLOB_RECURSE _source1 "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
-if(IOS OR TVOS)
-	file(GLOB_RECURSE _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
-	set(_MODULE_SOURCE ${_source1} ${_source2})
-endif()
-if(ANDROID)
-	set(_MODULE_SOURCE ${_source1})
+	message(STATUS "_MODULE_HEADERS : ${_MODULE_HEADERS}")
 endif()
 
-#set(KALTURA_PLAYER_ANDROID_PATH ${CMAKE_CURRENT_LIST_DIR})
+file(GLOB_RECURSE _source1 "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
+
+if(IOS OR TVOS)
+	file(GLOB_RECURSE _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
+	file(GLOB_RECURSE _source3 "${_MODULE_PATH}/pk/ios/*.cpp")
+	set(_MODULE_SOURCE ${_source1} ${_source2} ${_source3})
+endif()
+
+if(ANDROID)
+	file(GLOB_RECURSE _source4 "${_MODULE_PATH}/pk/android/*.cpp")
+	set(_MODULE_SOURCE ${_source1} ${_source4})
+	message(STATUS "_MODULE_SOURCE : ${_MODULE_SOURCE}")
+endif()
 
 # Make sure the specific platform include is processed
 if(IOS)
@@ -31,18 +38,17 @@ endif()
 if(TVOS)
     include(${CMAKE_CURRENT_LIST_DIR}/youi/cmake/Platform/YiTvos.cmake)
 endif()
-if (ANDROID)
-#    include(${CMAKE_CURRENT_LIST_DIR}/android)
-endif()
 
-target_sources(${PROJECT_NAME} PRIVATE
-    ${_MODULE_HEADERS}
-    ${_MODULE_SOURCE}
+message(STATUS "PROJECT_NAME : ${YI_PROJECT_NAME}")
+
+target_sources(${YI_PROJECT_NAME} PRIVATE
+	${_MODULE_HEADERS}
+	${_MODULE_SOURCE}
 )
 
 # make headers available for linking the project
-target_include_directories(${PROJECT_NAME}
-    PRIVATE ${_MODULE_PATH}
+target_include_directories(${YI_PROJECT_NAME}
+	PRIVATE ${_MODULE_PATH}
 )
 
 message(STATUS "_MODULE_PATH: ${_MODULE_PATH}")
--- kaltura-player-video-component/README.md
+++ kaltura-player-video-component/README.md
@@ -1,1 +1,5 @@
-# kaltura-player-frameworks
+# video-component
+
+The `ModuleList.cmake` file was updated yesterday (2021-01-19) to:
+- properly set the header and source file lists for an Android build
+- update the header and source file lists for iOS and tvOS builds
