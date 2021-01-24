868e8e808dc7707bf97aed89af8b6096881e9935
Nilit Danan <nilit.danan@kaltura.com>
2021-01-24 20:02:42 +0200

    Removed the 'AFNetworking' from the pod files, it is now embedded in the SmartLib pod as should be. Fixed the ModuleList.cmake file.

--- ModuleList.cmake
+++ ModuleList.cmake
@@ -6,31 +6,28 @@ set(_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/youi/src")
 set(KALTURA_PLAYER_JAVA_SRC_PATH ${CMAKE_CURRENT_LIST_DIR})
 set(KALTURA_PLAYER_FRAMEWORKS_PATH ${CMAKE_CURRENT_LIST_DIR}) 
 
-file(GLOB_RECURSE _headers1 "${_MODULE_PATH}/nativemodule/*.h" "${_MODULE_PATH}/pk/*.h")
+
+
+file(GLOB _commonheaders "${_MODULE_PATH}/nativemodule/*.h" "${_MODULE_PATH}/pk/*.h" "${_MODULE_PATH}/player/*.h")
+file(GLOB _commonsource "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
 
 if(IOS OR TVOS)
-	file(GLOB_RECURSE _headers2 "${_MODULE_PATH}/player/ios/*.h")
-	set(_MODULE_HEADERS ${_headers1} ${_headers2})
+	file(GLOB _appleheaders "${_MODULE_PATH}/player/ios/*.h")
+	set(_MODULE_HEADERS ${_commonheaders} ${_appleheaders})
+
+ 	file(GLOB _applesource "${_MODULE_PATH}/pk/ios/*.mm" "${_MODULE_PATH}/player/ios/*.mm" "${_MODULE_PATH}/player/ios/*.m")
+	set(_MODULE_SOURCE ${_commonsource} ${_applesource})
 endif()
+
 
 if(ANDROID)
-	set(_MODULE_HEADERS ${_headers1})
-	message(STATUS "_MODULE_HEADERS : ${_MODULE_HEADERS}")
+	set(_MODULE_HEADERS ${_commonheaders})
+
+	file(GLOB _androidsource "${_MODULE_PATH}/pk/android/*.cpp" "${_MODULE_PATH}/player/android/*.cpp")
+	set(_MODULE_SOURCE ${_commonsource} ${_androidsource})
 endif()
 
-file(GLOB_RECURSE _source1 "${_MODULE_PATH}/nativemodule/*.cpp" "${_MODULE_PATH}/player/*.cpp")
 
-if(IOS OR TVOS)
-	file(GLOB_RECURSE _source2 "${_MODULE_PATH}/player/ios/*.m" "${_MODULE_PATH}/player/ios/*.mm")
-	file(GLOB_RECURSE _source3 "${_MODULE_PATH}/pk/ios/*.cpp")
-	set(_MODULE_SOURCE ${_source1} ${_source2} ${_source3})
-endif()
-
-if(ANDROID)
-	file(GLOB_RECURSE _source4 "${_MODULE_PATH}/pk/android/*.cpp")
-	set(_MODULE_SOURCE ${_source1} ${_source4})
-	message(STATUS "_MODULE_SOURCE : ${_MODULE_SOURCE}")
-endif()
 
 # Make sure the specific platform include is processed
 if(IOS)
@@ -47,7 +44,7 @@ target_sources(${YI_PROJECT_NAME} PRIVATE
 	${_MODULE_SOURCE}
 )
 
-# make headers available for linking the project
+# Make headers available for linking the project
 target_include_directories(${YI_PROJECT_NAME}
 	PRIVATE ${_MODULE_PATH}
 )
