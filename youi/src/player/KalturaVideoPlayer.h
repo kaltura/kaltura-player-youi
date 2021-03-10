bee54eaba5d0104cfb990cc13d7e8da322fc9e21
Gilad Nadav <gilad.nadav@kaltura.com>
2021-03-10 15:11:34 +0200

    Update KalturaVideoPlayer.h

--- youi/src/player/KalturaVideoPlayer.h
+++ youi/src/player/KalturaVideoPlayer.h
@@ -162,9 +162,6 @@ private:
     std::vector<KalturaClosedCaptionTrack> m_closedCaptionsTracks;
     int32_t m_selectedClosedCaptionTrack = -1;
 
-    std::vector<KalturaVideoPlayer::ImageTrackInfo> m_imageTracks;
-    int32_t m_selectedImageTrack = -1;
-
     std::vector<CYIAbstractVideoPlayer::SeekableRange> m_liveSeekableRanges;
 
     YI_TYPE_BASES(KalturaVideoPlayer, CYIAbstractVideoPlayer)
